9dda6178917f7db3ade20b7effae4abf114c3930
Tomasz Rybarczyk <paluho@gmail.com>
2018-12-03 23:08:29 +0100

    Use explicit imports format for re-exported modules.

--- src/Snail.purs
+++ src/Snail.purs
@@ -17,14 +17,14 @@ import Data.Either (either)
 import Effect.Aff (runAff)
 import Effect.Class.Console (error)
 import Effect.Exception (message)
-import Snail.Console as Console
-import Snail.Control as Control
-import Snail.Env as Env
-import Snail.File as File
-import Snail.OS as OS
-import Snail.Path as Path
-import Snail.Process as Process
-import Snail.Types as Types
+import Snail.Console (echo, err) as Console
+import Snail.Control (andandand, exists, existsCheck, fromEither, fromJust, fromMaybe, loop, mIf, notExistsCheck, ororor, sleep, when, (&&&), (|||), (~?>), (~~>)) as Control
+import Snail.Env (Optional(..), getVar, printEnv, setVar) as Env
+import Snail.File (appendFile, cat, chmod, cp, ls, mkdir, mv, prependFile, rm, rmdir, touch, writeFile, (+>), (>>)) as File
+import Snail.OS (arch, cpus, endianness, freemem, home, hostname, loadavg, networkInterfaces, ostype, platform, release, tmp, totalmem, uptime) as OS
+import Snail.Path (chdir, pathpend, (</>)) as Path
+import Snail.Process (args, exec, exit, exitWith, fork, input, params, raw, run, (!?)) as Process
+import Snail.Types (class Address, FILE, FOLDER, File, Folder, Script, Snail, file, folder, getAddress, runFile, runFolder) as Types
 
 -- | Runs a Snail computation
 crawl :: forall a. Types.Snail a -> Types.Script Unit
